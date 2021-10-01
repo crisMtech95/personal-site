ffc5f0afdbffb12922b080cb85d153ad5e4dfae1
crisMtech95 <cmg.95.col@hotmail.com>
2021-10-01 17:41:14 -0400

    changes to favicon, title, projects gifs, content.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,11 +7,14 @@ export default function About() {
     return(
         <div className="About__mainContainer" id="about">
             <div className="About__container">
-                <div className="About__imageDiv">
-                    <div className="About__image" />
-                </div>
-                <div className="About__contentDiv">
-                    <p>I'm originally from Colombia, but I am currently based in New York City. My love for problem solving and learning new technologies have driven to learn full stack software developement. </p>
+                <h2>About</h2>
+                <div className="About__content">
+                    <div className="About__imageDiv">
+                        <div className="About__image" />
+                    </div>
+                    <div className="About__contentDiv">
+                        <p>I'm originally from Colombia, but I am currently based in New York City. My love for problem solving and learning new technologies have driven to learn full stack software developement. </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -18,19 +18,52 @@ export default function Projects() {
                 <div className="Projects__container">
                     <div className="Projects__SingleProjectDiv">
                         <h3>All-bnb</h3>
-                        <div className="Projects__imageDiv allbnbImg"></div>
+                        <div className="Projects__SingleProjectMain">
+                            <a className="Projects__SingleProjectsLinks" href="https://all-bnb.herokuapp.com/" target="_blank">
+                                <div className="Projects__imageDiv allbnbImg"></div>
+                            </a>
+                            <div className="Projects__contentMain">
+                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
+                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                            </div>
+                        </div>
                     </div>
                     <div className="Projects__SingleProjectDiv">
                         <h3>Audify</h3>
-                        <div className="Projects__imageDiv audifyImg"></div>
+                        <div className="Projects__SingleProjectMain">
+
+                            <div className="Projects__contentMain">
+                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
+                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                            </div>
+                            <a className="Projects__SingleProjectsLinks" href="https://audify-app.herokuapp.com/" target="_blank">
+                                <div className="Projects__imageDiv audifyImg"></div>
+                            </a>
+                        </div>
                     </div>
                     <div className="Projects__SingleProjectDiv">
                         <h3>Sportsgram</h3>
-                        <div className="Projects__imageDiv sportsgramImg"></div>
+                        <div className="Projects__SingleProjectMain">
+                            <a className="Projects__SingleProjectsLinks" href="https://flickstagram.herokuapp.com/" target="_blank">
+                                <div className="Projects__imageDiv sportsgramImg"></div>
+                            </a>
+                            <div className="Projects__contentMain">
+                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
+                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                            </div>
+                        </div>
                     </div>
                     <div className="Projects__SingleProjectDiv">
                         <h3>Stack-OilFlow</h3>
-                        <div className="Projects__imageDiv stack-oilFlowImg"></div>
+                        <div className="Projects__SingleProjectMain">
+                            <div className="Projects__contentMain">
+                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
+                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                            </div>
+                            <a className="Projects__SingleProjectsLinks" href="http://stack-oil-flow.herokuapp.com/" target="_blank">
+                                <div className="Projects__imageDiv stack-oilFlowImg"></div>
+                            </a>
+                        </div>
                     </div>
                 <div/>
                 </div>
