869ecb0dd5e090af942ba58f9829faef43039567
crisMtech95 <cmg.95.col@hotmail.com>
2021-10-01 19:06:36 -0400

    project changes and added education component

---
+++ src/components/Education/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link, NavLink } from 'react-router-dom';
+import './Education.css'
+
+export default function Education() {
+
+
+    return(
+        <div className="Education__mainContainer" id="Education">
+            <div className="Education__container">
+
+
+            </div>
+        </div>
+    )
+}
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -24,7 +24,7 @@ export default function Projects() {
                             </a>
                             <div className="Projects__contentMain">
                                 <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
-                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                                <p><strong>-Technologies used:</strong> JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
                             </div>
                         </div>
                     </div>
@@ -33,8 +33,8 @@ export default function Projects() {
                         <div className="Projects__SingleProjectMain">
 
                             <div className="Projects__contentMain">
-                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
-                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                                <p> Audify is an application that utilizes the Spotify-API to stream music and uses full-stack technology to allow for playlist creations where a user can store songs they like. Inspired by Spotify, the application uses React and Redux to provide the single-page-application experience and Flask in the backend to interact with the PostgreSQL Database.</p>
+                                <p><strong>-Technologies used:</strong> JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
                             </div>
                             <a className="Projects__SingleProjectsLinks" href="https://audify-app.herokuapp.com/" target="_blank">
                                 <div className="Projects__imageDiv audifyImg"></div>
@@ -48,8 +48,8 @@ export default function Projects() {
                                 <div className="Projects__imageDiv sportsgramImg"></div>
                             </a>
                             <div className="Projects__contentMain">
-                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
-                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                                <p>Sportsgram is a full Stack clone of Flicker, with a feed inspired by instagram where user can find, comment and share their favorite sports related stories.</p>
+                                <p><strong>-Technologies used:</strong> Express, Sequelize, React, Redux, Javascript, Html, Css, Postgresql</p>
                             </div>
                         </div>
                     </div>
@@ -57,8 +57,8 @@ export default function Projects() {
                         <h3>Stack-OilFlow</h3>
                         <div className="Projects__SingleProjectMain">
                             <div className="Projects__contentMain">
-                                <p> As you might deduce from the name, Allbnb is a full stack clone of Airbnb with the very important difference that users are able to lease or lend any type of item that you might need for a trip. Need a place to stay?, a car? or camping gear for your trip?, Allbnb is the place where you can find all those items for a specific location.</p>
-                                <p>-Technologies used: JavaScript, Html, Css, React, Redux, Python, Flask, SQLAlchemy, Postgresql. </p>
+                                <p>Stack Oil flow is a social based platform that allows users to utilize question and answer based features to both share and gain Automotive based knowledge. The main goal of Stack Oil Flow is to cultivate an environment of automotive enthusiasts, both recreational and professional to come together over a shared passion.</p>
+                                <p><strong>-Technologies used:</strong> JavaScript, Html, Css, Pug, Express, Sequelize.</p>
                             </div>
                             <a className="Projects__SingleProjectsLinks" href="http://stack-oil-flow.herokuapp.com/" target="_blank">
                                 <div className="Projects__imageDiv stack-oilFlowImg"></div>
